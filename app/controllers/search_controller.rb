7825c402121cebaa4660bbf8688da4d047d0564c
Wren Steitle <lauren.steitle@gmail.com>
2020-02-10 10:55:05 -0700

    I wish I had five more minutes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,31 +1,33 @@
 class SearchController < ApplicationController
 
   def index
-    mykey = '$2a$10$Xlmp/Ne7Sk5kKfhVXGGu0OjIJ0ne6toxarurb6Zo0B9NzWy2Pwy6y'
     conn = Faraday.new('https://www.potterapi.com/v1/')
-    houses_response = conn.get("houses?key=#{mykey}")
-    houses_hash = JSON.parse(houses_response.body)
-    houses = Hash.new
-    houses_hash.map do |house_info|
-      houses[house_info["name"]] = house_info["_id"]
-    end
-    search_house = houses[params[:house]]
-    @house_name = params["house"]
-    
-    members_response = conn.get("houses/#{search_house}?key=#{mykey}")
-    members_parsed = JSON.parse(members_response.body)
-    members = members_parsed[0]["members"]
-    @order = []
-    members.each do |k,v|
-
-      studentresponse = conn.get("characters/#{k["id"]}?key=#{mykey}")
-      student = JSON.parse(studentresponse.body)
-      student.map do |student|
-        if student["orderOfThePhoenix"] == true
-          @order << student
-        end
-      end
-      @order
-    end
+    # houses_response = conn.get("houses?key=#{mykey}")
+    # houses_hash = JSON.parse(houses_response.body)
+    # houses = Hash.new
+    # houses_hash.map do |house_info|
+    #   houses[house_info["name"]] = house_info["_id"]
+    # end
+    # search_house = houses[params[:house]]
+     @house_name = params["house"]
+    #
+    # members_response = conn.get("houses/#{search_house}?key=#{mykey}")
+    # members_parsed = JSON.parse(members_response.body)
+    # members = members_parsed[0]["members"]
+    # @order = []
+    # members.each do |k,v|
+    #
+    #   studentresponse = conn.get("characters/#{k["id"]}?key=#{mykey}")
+    #   student = JSON.parse(studentresponse.body)
+    #   student.map do |student|
+    #     if student["orderOfThePhoenix"] == true
+    #       @order << student
+    #     end
+    #   end
+    #   @order
+    # end
+    #just about to refactor to this:
+    order_resp = conn.get("characters?orderOfThePhoenix=true&key=ENV[HP_KEY]")
+    @order = JSON.parse(order_resp.body)[0]
   end
 end

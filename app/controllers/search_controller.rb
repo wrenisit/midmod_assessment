ffc019b9d3b12754a6362f978a6293241bb2e615
Wren Steitle <lauren.steitle@gmail.com>
2020-02-10 10:27:28 -0700

    In process

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,10 +5,27 @@ class SearchController < ApplicationController
     conn = Faraday.new('https://www.potterapi.com/v1/')
     houses_response = conn.get("houses?key=#{mykey}")
     houses_hash = JSON.parse(houses_response.body)
-    houses_hash.each do |house_info|
-      House.create(house_info)
+    houses = Hash.new
+    houses_hash.map do |house_info|
+      houses[house_info["name"]] = house_info["_id"]
     end
-    search_house = House.where(name: params[:house]).first
+    search_house = houses[params[:house]]
+    members_response = conn.get("houses/#{search_house}?key=#{mykey}")
+    members_parsed = JSON.parse(members_response.body)
+    members = members_parsed[0]["members"]
+    @order = []
+    members.each do |k,v|
+
+      studentresponse = conn.get("characters/#{k["id"]}?key=#{mykey}")
+      student = JSON.parse(studentresponse.body)
+      student.map do |student|
+        binding.pry
+        if student[0]["orderOfThePhoenix"] == true
+          @order << student[0]
+        end
+      end
+      @order
+    end
     binding.pry
   end
 
--- app/poros/house.rb
+++ app/poros/house.rb
@@ -1,7 +1,6 @@
-class House
-
+class House < ApplicationRecord
+  attr_reader :name, :house_id
   def initialize(info)
-    binding.pry
     @name = info["name"]
     @house_id = info["_id"]
   end
